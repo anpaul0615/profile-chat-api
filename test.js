7e337d6811c2cb9752444d26839252210c1087c8
PAUL AN <anpaul0615@gmail.com>
2018-07-03 18:10:29 +0900

    Change local test port

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import App from './src/App';
 
-App.listen(3000, function(err){
+App.listen(5000, function(err){
     if(err) console.log(err);
-    else console.log('[test.js] Listen 3000 OK!!');
+    else console.log('[test.js] Listen 5000 OK!!');
 });

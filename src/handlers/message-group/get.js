ab150694285bf5d73b3239d359946545aa1b8a63
PAUL AN <anpaul0615@gmail.com>
2018-09-24 20:08:57 +0900

    Implement "get message group" (HTTP GET /messages/group)

---
+++ src/handlers/message-group/get.js
@@ -0,0 +1,59 @@
+import * as response from '../../helpers/response';
+import * as dynamodb from '../../helpers/dynamodb';
+
+/**
+ * HTTP GET /messages/group :: Get Message Group
+ * 
+ * @param {*} event 
+ * @param {*} context 
+ * @param {*} callback 
+ */
+export const handler = async (event, context, callback)=>{
+    /** Immediate response for WarmUP plugin */
+    if (event.source === 'serverless-plugin-warmup') {
+        console.log('[message-group.get.handler] warmup ok!!');
+        return callback(null, 'message-group.get.handler is warm!!');
+    }
+
+    // Recieve Request Data
+    const queryStringData = event.queryStringParameters;
+    if (!queryStringData) {
+        return callback(null, response.badRequest({
+            status: false,
+            message: 'Missing querystring..! ' }));
+    }
+    const { groupname } = queryStringData;
+    if (!groupname) {
+        return callback(null, response.badRequest({
+            status: false,
+            message: 'Missing groupname..! ' }));
+    }
+
+    // Get Message Group
+    try {
+        const params = {
+            TableName: 'profile-chat.MessageGroups',
+            Key: {
+                'groupname': groupname
+            }
+        };
+        const result = await dynamodb.call('get', params);
+
+        if (!result.Item) {
+            return callback(null, response.notFound({
+                status: false,
+                message: `no message-group found..!` }));
+        }
+
+        return callback(null, response.success({
+            status: true,
+            message: `message-group is found!!`,
+            data: result.Item }));
+
+    } catch (e) {
+        console.log(e);
+        return callback(null, response.failure({
+            status: false,
+            message: 'Internal error..!' }));
+    }
+};
